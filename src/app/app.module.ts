c533163882dbc092c31bbd6a4dd24334439f8f82
HalaszPeter1 <halasz.peter1@students.jedlik.eu>
2022-10-14 11:33:30 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginAblakComponent } from './login-ablak/login-ablak.component';
+import { FejlecComponent } from './fejlec/fejlec.component';
+import { LablecComponent } from './lablec/lablec.component';
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginAblakComponent
+    LoginAblakComponent,
+    FejlecComponent,
+    LablecComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login-ablak/login-ablak.component.ts
+++ src/app/login-ablak/login-ablak.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginAblakComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public lblUsername:string = "Felhasználónév:"
+  public lblPassword:string = "Jelszó:"
+  public lblSubmit:string = "Belépés"
+  public lblReset:string = "Adatok törlése"
+  public username:string = "";
+  public password:string = "";
+  public error:string = "";
+  public loggedin: boolean = false;
+  constructor() {
+    
   }
 
+  ngOnInit(): void {
+  this.lblUsername = "Username:";
+    this.lblPassword = "Password:";
+    this.lblSubmit = "Login";
+    this.lblReset = "Reset";
+  }
+  kattint(){
+    this.error = "";
+    this.loggedin = false;
+    if (this.username == "admin" && this.password == "123") {
+      alert("Beléptél: "+this.username)
+      this.loggedin = true;
+    }
+    else{
+      this.error = "Hibás felhasználónév vagy jelszó!";
+
+    }
+  }
+  
+  logout(){
+    this.loggedin= false;
+    this.error = "";
+    this.password = "";
+  }
+  
 }
+
